72c0d0718dcaf8397dfef2ecd96e6f6cb9d1d408
omarino-go <moulomar9099@gmail.com>
2025-05-16 00:08:45 +0100

    Update cli_arguments.sh

--- cli_arguments.sh
+++ cli_arguments.sh
@@ -2,6 +2,7 @@
 #recuperer l'argument
 read -a arg
 i=1
-# for args in "$arg[@]";then
-#   echo "argument $((i++)) : $args "
-# done
+for args in "$arg[@]";then
+    echo "argument $i : $args "
+    ((i++))
+done
