49bb103abf42c3c7e24392f722fbc0314fbaf199
omarino-go <moulomar9099@gmail.com>
2025-05-17 00:00:55 +0100

    Update find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -2,8 +2,10 @@
 #recupere le nom du fichier et la chaine à cherche
 read fichier
 read chaine
+#recherche de la chaine dans le fichier
 if grep -q "$chaine" "$fichier" ; then
+  #s'il existe
   echo "La chaîne '$chaine' a été trouvée dans $fichier."
-  else
+  else #sinon
   echo "La chaîne '$chaine' n'a pas été trouvée dans $fichier."
 fi
