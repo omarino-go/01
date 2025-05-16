3aca67b7dea97f74c5e3072854f4bf5902dd58b6
omarino-go <moulomar9099@gmail.com>
2025-05-16 22:25:40 +0100

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -6,12 +6,16 @@ case $o in
     ;;
   -)
     resultat=$((a-b))
+    ;;
   \* || x)
     resultat=$((a*b))
+    ;;
   /)
     resultat=$((a/b))
+    ;;
   *)
     echo "L'operateur n'existe pas"
     exit 1
+    ;;
 esac
 echo "Résultat : $resultat"
