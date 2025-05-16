b359b20c545609d26f29b4ae4dc7883e63694492
omarino-go <moulomar9099@gmail.com>
2025-05-16 23:58:58 +0100

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
+#recupere les differents donnée necessaire
 read a b o
+# selon l'operateur
 case $o in
+  #cas +
   +)
     resultat=$((a+b))
     ;;
+  #cas -
   -)
     resultat=$((a-b))
     ;;
+  #cas x
   \*)
     resultat=$((a*b))
     ;;
   x)
     resultat=$((a*b))
     ;;
+  #cas /
   /)
     resultat=$((a/b))
     ;;
+  #sinon et arret du programme
   *)
     echo "L'operateur n'existe pas"
     exit 1
     ;;
 esac
+#affiche du resultat
 echo "Résultat : $resultat"
